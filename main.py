84f45174f28dc6e06ca4d2aaf57ceed9fc819b21
Feng Chun <springfeng@gmail.com>
2020-10-27 20:30:05 +0800

    koufu

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup("<html>a web page</html>", 'html.parser')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
+
 def main():
-    print("main starting")
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
-    response = requests.get("http://investor.koufu.com.sg/newsroom.html", headers=headers)
+    print("Getting koufu")
+    koufu()
+
+def koufu():
+    url_koufu = 'http://investor.koufu.com.sg'
+    response = requests.get(url_koufu + "/newsroom.html", headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
-    print(soup.prettify())
-    soup.findAll('div', {'class': 'ir_home_news_list'})
+    news = soup.findAll('div', {'class': 'ir_home_news_list'})
+    link = news[0].find('a')
+    title = link.text
+    news_url = link['href']
+    response = requests.get(url_koufu + news_url, headers=headers)
+    print(url_koufu + news_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    for a in soup.select('a[href$=".pdf"]'):
+        r = requests.get(url_koufu + a['href'], allow_redirects=True)
+        open(a['href'].split('/')[-1], 'wb').write(r.content)
+
 if __name__ == "__main__":
     main() 
