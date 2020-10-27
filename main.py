c71b1e74f274596aefc2fa9f9bb9c7ca1722e0cb
Feng Chun <springfeng@gmail.com>
2020-10-27 20:48:50 +0800

    add kimly

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 def main():
     print("Getting koufu")
-    koufu()
+    #koufu()
+    kimly()
 
 def koufu():
     url_koufu = 'http://investor.koufu.com.sg'
@@ -21,6 +22,21 @@ def koufu():
     for a in soup.select('a[href$=".pdf"]'):
         r = requests.get(url_koufu + a['href'], allow_redirects=True)
         open(a['href'].split('/')[-1], 'wb').write(r.content)
+
+def kimly():
+    url_kimly = 'https://sginvestors.io/sgx/stock/1d0-kimly/company-announcement'
+    response = requests.get(url_kimly , headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    #print(soup.prettify())
+    news = soup.findAll('div', {'class': 'corpannouncementitem list-group-item'})
+    #print(news[0])
+    response = requests.get(news[1].a['href'] , headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    for a in soup.select('a[href$=".pdf"]'):
+        print(a['href'])
+        r = requests.get("https://links.sgx.com"+a['href'], allow_redirects=True)
+        open(a['href'].split('/')[-1], 'wb').write(r.content)
+    #print(news)
 
 if __name__ == "__main__":
     main() 
